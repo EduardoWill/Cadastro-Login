c8a4cbf2a93397fabd0bdfc6b4ea3394da53cc92
EduardoWill <eduardo.william188@gmail.com>
2025-06-06 16:45:26 -0300

    Check quase pronto

--- script.js
+++ script.js
@@ -9,22 +9,32 @@ form.addEventListener("submit", (event) =>{
     checkUsername()
 
 })
-
+//Function para checar se existe algo no campo usuário
 function checkUsername(){
     const usuarioValor = usuario.value
-    
+    //Condicional para ver se está vazia
     if (usuarioValor === ""){
       errorInput(usuario, "Preencha este campo")
        
     }
-function errorInput(input, message){
+//Function que faz o negócio de mensagem de erro
+function errorInput(input, mensagem){
+      //.parentElemente puxa a Tag
      const formItem = input.parentElement
      const textMessage = formItem.querySelector('p')
 
-     textMessage.innerText = message
+     textMessage.innerText = mensagem
 
      formItem.className = "botao-campo error"
 }
+
+}
+
+function checkEmail(){
+  const emailValor = email.value
+  if (emailValor === ""){
+     
+  }
 
 }
 
